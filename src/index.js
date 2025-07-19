a081eea1e44d62089cf81b5e67432eefdce249bb
eustaquio <eustaquio.galano@gmail.com>
2025-07-20 01:12:03 +0800

    feat: import click event listeners

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import '@fontsource/kanit';
 import './styles.css';
+import { setUpClickEvents } from './scripts/events';
+
+document.addEventListener('DOMContentLoaded', () => {
+  setUpClickEvents();
+});
