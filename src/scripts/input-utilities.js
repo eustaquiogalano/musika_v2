46b73959ea2c93fe4206f3891b49b07ec595336e
eustaquio <eustaquio.galano@gmail.com>
2025-08-06 15:09:30 +0800

    feat: add utilities for input
    - add a function to clear all the inputs after submitting the form

---
+++ src/scripts/input-utilities.js
@@ -0,0 +1,8 @@
+export function clearSignUpInputs() {
+  document.querySelector('#username').value = '';
+  document.querySelector('#first-name').value = '';
+  document.querySelector('#last-name').value = '';
+  document.querySelector('#birthdate').value = '';
+  document.querySelector('#e-mail').value = '';
+  // document.querySelector('#').value = '';
+}
